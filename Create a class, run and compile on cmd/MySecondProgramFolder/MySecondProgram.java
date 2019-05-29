0a7c64ad0d143ee002e3b711353239dd4a260ebd
Ess-B <51133968+Ess-B@users.noreply.github.com>
2019-05-29 20:25:32 +0300

    Add files via upload

---
+++ Create a class, run and compile on cmd/MySecondProgramFolder/MySecondProgram.java
@@ -0,0 +1,36 @@
+class MySecondProgram{
+	public static void main(String[] args){
+		System.out.println("Hello, Essie!");
+		
+				
+		
+	}
+
+
+}
+/* To complile program on cmd without installing java just open cmd and:
+(1)Navigate to the folder where file is located
+(2)Type or paste the location of the jdk/bin files and enclose them with quotes ("")
+(3)now write javac NameOfFile.java
+
+**To run the program:
+(A) If you haven't installed java to the system, follow method 1 above. 
+		Remove step (3)then skip to step (C) instead
+(B) If you have, just navigate to the file location on cmd, then: 
+
+(C) Type java after the '>' , don't type javac (java compiler)
+(D) Type name of java file but don't add the extension name.
+
+Example: To compile this doc,
+
+C:\Users\ADMIN\Documents\Notepad programs\MySecondProgramFolder>javac MySecondProgram.java
+
+Output: C:\Users\ADMIN\Documents\Notepad programs\MySecondProgramFolder>
+(It won't print anything, just brings another line showing the directory you had navigated)
+
+Example 2: To run this doc/file/program:
+C:\Users\ADMIN\Documents\Notepad programs\MySecondProgramFolder>java MySecondProgram
+
+Output: Hello, Essie!
+
+*/
